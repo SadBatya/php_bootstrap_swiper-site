2ec2a8c642403864710385229f0bc9a581bfeec2
sadbatya <sadbatya@mail.ru>
2023-12-05 20:11:53 -0500

    добавил боксы и хедер 5 dec 20:11

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>Your Website</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+              <li>sdasdasd</li>
+              <li>sdasdasd</li>
+              <li>sdasdasd</li>
+              <li>sdasdasd</li>
+            </ul>
+        </nav>
+    </header>
