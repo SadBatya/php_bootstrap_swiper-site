4d52cf449df4481e285b15d970d4da83448f6c9b
sadbatya <sadbatya@mail.ru>
2023-12-06 17:46:38 -0500

    сделал слайдер без адаптива

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const slider = document.getElementById('slider');
-  const prevButton = document.getElementById('prev');
-  const nextButton = document.getElementById('next');
-  const slideWidth = document.querySelector('.slide').offsetWidth;
+let offset = 0 //смещение от левого края
+const sliderLine = document.querySelector('.slider_line')
 
-  let currentIndex = 0;
-
-  nextButton.addEventListener('click', function () {
-    if (currentIndex < slider.children.length - 1) {
-      currentIndex++;
-    } else {
-      currentIndex = 0;
-    }
-    updateSlider();
-  });
-
-  prevButton.addEventListener('click', function () {
-    if (currentIndex > 0) {
-      currentIndex--;
-    } else {
-      currentIndex = slider.children.length - 1;
-    }
-    updateSlider();
-  });
-
-  function updateSlider() {
-    const newTransformValue = -currentIndex * slideWidth;
-    slider.style.transform = `translateX(${newTransformValue}px)`;
+document.querySelector('#next').addEventListener('click', () => {
+  offset += 1024 
+  if(offset > 2048){
+    offset = 0
   }
-});
+  sliderLine.style.left = -offset + 'px'
+})
+
+document.querySelector('#prev').addEventListener('click', () => {
+  offset -= 1024
+  if(offset < 0){
+    offset = 2048
+  }
+  sliderLine.style.left = -offset + 'px'
+})
