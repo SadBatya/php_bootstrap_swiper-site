309697920d3fa344febb75a94b12182c37b57d3a
sadbatya <sadbatya@mail.ru>
2023-12-07 16:31:01 -0500

    fix bigs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
-import { burgerMenu } from './burgerMenu';
-import { initSwiper } from './swiperConfig';
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 1, // Количество видимых слайдов
+  spaceBetween: 20,
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  autoHeight: true,
 
-burgerMenu();
-initSwiper();
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const burgerMenu = () => {
+  const burgerButton = document.querySelector('.burger_button');
+  const body = document.querySelector('body')
+  const menu = document.querySelector('.burger_menu');
+  
+  burgerButton.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    body.classList.toggle('stop-scroll')
+  });
+};
+
+burgerMenu()
+
